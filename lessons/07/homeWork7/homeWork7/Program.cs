09f828d60aaef8a65fb75f349baa48b560b133af
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-01 16:43:17 +0300

    ДЗ-7(1)

---
+++ lessons/07/homeWork7/homeWork7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace homeWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать консольное приложение, которое запрашивает строку и выводит количество слов, начинающихся на букву A
+            //Программа должна спрашивать исходную строку до тех пор, пока пользователь не введёт хотя бы 2 слова.
+
+            
+            string[] arr;
+
+            for (int i = 0; ; i++)
+            {
+                Console.Write("Enter a text consisting of at least 2 words: ");
+                string str = Console.ReadLine();
+                arr = str.Split(' ', '\t' , StringSplitOptions.RemoveEmptyEntries);
+                if (arr.Length == 0 || arr.Length == 1)
+                {
+                    continue;
+                }
+                break;
+            }
+
+            int counter = default;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].StartsWith("A"))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
