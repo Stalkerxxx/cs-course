6d641b81418af6ee1fe71a62351997f0a8be3b61
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-11 02:01:48 +0300

    ДЗ-10

---
+++ lessons/10/homeWork10/homeWork10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] arrPerson = new Person[3];
+            for (int i = 0; i < arrPerson.Length; i++)
+            {
+                Console.WriteLine("Enter the name of the user: ");
+                string name = Console.ReadLine();
+                if (name == "" || name == null)
+                {
+                    Console.WriteLine("You entered an empty string");
+                    break;
+                }
+                Console.WriteLine("Enter the age of the user:");
+                byte age = byte.Parse(Console.ReadLine());
+                arrPerson[i] = new Person
+                {
+                    Name = name,
+                    Age = age
+                };
+
+                Console.WriteLine(arrPerson[i].Discription);
+            }
+            Console.ReadKey();
+        }
+    }
+}
