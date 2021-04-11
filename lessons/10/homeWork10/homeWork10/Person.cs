8a70b75dc8de300c59b30b452e127aab94363816
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-11 22:49:32 +0300

    класс Person

---
+++ lessons/10/homeWork10/homeWork10/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace homeWork10
+{
+    class Person 
+    {
+        public string Name { get; set; } 
+        public byte Age { get; set; }
+        public string Discription =>
+            $"{Name} will be born in four years: {Age + 4}";
+    }
+}
