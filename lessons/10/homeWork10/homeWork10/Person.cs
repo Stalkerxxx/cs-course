fd758d10f3a47530f8a39e99c40e833c56622980
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-12 21:25:28 +0300

    класс Person

--- lessons/10/homeWork10/homeWork10/Person.cs
+++ lessons/10/homeWork10/homeWork10/Person.cs
@@ -4,6 +4,11 @@
     {
         public string Name { get; set; } 
         public byte Age { get; set; }
+        public byte AgeFourYears() 
+        {
+            var age = (byte)(Age + 4);
+            return age;
+        }
         public string Discription =>
             $"{Name} will be born in four years: {Age + 4}";
     }
