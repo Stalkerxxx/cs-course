816aca8df4434e33b20aa656b81ee347641aaa79
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-12 21:24:47 +0300

    ДЗ-10 исправленное

--- lessons/10/homeWork10/homeWork10/Program.cs
+++ lessons/10/homeWork10/homeWork10/Program.cs
@@ -6,24 +6,47 @@ namespace homeWork10
     {
         static void Main(string[] args)
         {
+
             Person[] arrPerson = new Person[3];
             for (int i = 0; i < arrPerson.Length; i++)
             {
-                Console.WriteLine("Enter the name of the user: ");
-                string name = Console.ReadLine();
-                if (name == "" || name == null)
+                string name;
+                byte age;
+                while (true)
                 {
-                    Console.WriteLine("You entered an empty string");
+                    Console.WriteLine("Enter the name of the user: ");
+                    name = Console.ReadLine();
+
+                    if (name == "" || name == null)
+                    {
+                        Console.WriteLine("You entered an empty string");
+                        continue;
+                    }
+                    Console.WriteLine("Enter the age of the user:");
+                    try
+                    {
+                        age = byte.Parse(Console.ReadLine());
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("You entered too many number \n Try again");
+                        continue;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("You entered not a number \n Try again");
+                        continue;
+                    }
                     break;
                 }
-                Console.WriteLine("Enter the age of the user:");
-                byte age = byte.Parse(Console.ReadLine());
+
                 arrPerson[i] = new Person
                 {
                     Name = name,
                     Age = age
                 };
 
+                arrPerson[i].AgeFourYears();
                 Console.WriteLine(arrPerson[i].Discription);
             }
             Console.ReadKey();
