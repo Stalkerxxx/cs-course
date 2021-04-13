ec996a974092555a55674200f1aa8e495eca7762
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-13 14:23:54 +0300

    ДЗ-10 исправленное

--- lessons/10/homeWork10/homeWork10/Program.cs
+++ lessons/10/homeWork10/homeWork10/Program.cs
@@ -6,45 +6,18 @@ namespace homeWork10
     {
         static void Main(string[] args)
         {
-            string name;
-            byte age;
+            string name = "";
+            byte age = default;
             Person[] arrPerson = new Person[3];
             for (int i = 0; i < arrPerson.Length; i++)
             {
-                while (true)
-                {
-                    Console.WriteLine("Enter the name of the user: ");
-                    name = Console.ReadLine();
-
-                    if (name == "" || name == null)
-                    {
-                        Console.WriteLine("You entered an empty string");
-                        continue;
-                    }
-                    Console.WriteLine("Enter the age of the user:");
-                    try
-                    {
-                        age = byte.Parse(Console.ReadLine());
-                    }
-                    catch (OverflowException)
-                    {
-                        Console.WriteLine("You entered too many number \n Try again");
-                        continue;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("You entered not a number \n Try again");
-                        continue;
-                    }
-                    break;
-                }
-
                 arrPerson[i] = new Person
                 {
-                    Name = name,
-                    Age = age
+                    NameUser = name,
+                    AgeUser = age
                 };
-
+                arrPerson[i].Name();
+                arrPerson[i].Age();
                 Console.WriteLine(arrPerson[i].Discription);
             }
             Console.ReadKey();
