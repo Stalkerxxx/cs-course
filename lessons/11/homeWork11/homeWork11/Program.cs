00968304b7c593970f66491feaec1f0755f21e9f
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-16 14:29:05 +0300

    ДЗ-11

---
+++ lessons/11/homeWork11/homeWork11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace homeWork11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reminderltem firstSignal = new Reminderltem(
+                new DateTimeOffset(2021, 04, 17, 20, 20, 34, TimeSpan.Zero),
+                "WAKE UP"
+                );
+           
+            Console.WriteLine(firstSignal.WriteProperties);
+            
+        }
+    }
+}
