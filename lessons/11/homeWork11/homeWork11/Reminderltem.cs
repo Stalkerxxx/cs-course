33a7fbe7384315c4c3831dafe7e0113a8d95da1f
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-16 14:30:05 +0300

    Класс ReminderItem

---
+++ lessons/11/homeWork11/homeWork11/Reminderltem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homeWork11
+{
+    class Reminderltem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessange{ get; set; }
+        public TimeSpan TimeToAlarm => 
+            DateTimeOffset.UtcNow.Subtract(AlarmDate);
+        public bool IsOutdated =>
+            TimeToAlarm >= TimeSpan.Zero;
+        
+        public string WriteProperties =>
+            $"The alarm clock is set for: {AlarmDate}, {AlarmMessange}!!!," +
+            $" time to wake up:{TimeToAlarm.Days}:{TimeToAlarm.Hours}:{TimeToAlarm.Minutes}," +
+            $" {IsOutdated}";
+
+        public Reminderltem(DateTimeOffset alarmDate, string alarmMessange)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessange = alarmMessange;
+        }
+    }
+}
