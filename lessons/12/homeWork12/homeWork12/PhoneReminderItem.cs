7295595b8fe02c715d04cfd78d6dc537e4f6699a
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-19 23:09:36 +0300

    Класс PhoneNumber

---
+++ lessons/12/homeWork12/homeWork12/PhoneReminderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homeWork12
+{
+    class PhoneReminderItem : Reminderltem
+    {
+        public string PhoneNumber{ get; set; }
+        public PhoneReminderItem(
+            DateTimeOffset alarmDate,
+            string alarmMessange,
+            string phoneNumber
+            )
+            : base(alarmDate, alarmMessange)
+        {
+            PhoneNumber = phoneNumber;  
+        }
+        public override void WriteProperties()
+        {
+            Console.WriteLine($"PhoneReminderItem:" +
+                $" \nThe alarm clock is set for: {AlarmDate}, {AlarmMessange}," +
+                $" time to wake up:{TimeToAlarm.Days}:{TimeToAlarm.Hours}:{TimeToAlarm.Minutes}," +
+                $" {IsOutdated} and phone number: {PhoneNumber}");
+        }
+    }
+}
