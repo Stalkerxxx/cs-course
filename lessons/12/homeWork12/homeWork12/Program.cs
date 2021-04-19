d33a9614724628ea5c1138e9f6ea378b308b8ad5
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-19 23:05:03 +0300

    Дз-12

---
+++ lessons/12/homeWork12/homeWork12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homeWork12
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Создать лист объектов базового типа инициализированный как минимум 3-мя объектами разных типов.
+            //Вывести на экран их свойства WriteProperties().
+
+            List<Reminderltem> reminderltems = new List<Reminderltem>
+            {
+                new Reminderltem(
+                    new DateTimeOffset(
+                        2021, 04, 17, 20, 20, 34,
+                        TimeSpan.Zero
+                        ),
+                    "You overslept the alarm clock!!!"
+                    ),
+                new PhoneReminderItem(
+                new DateTimeOffset(
+                    2021, 04, 17, 20, 20, 34,
+                    TimeSpan.Zero
+                    ),
+                "You overslept the alarm clock!!!",
+                "+7(925)874-20-21"
+                ),
+                new ChatReminderItem(
+                    new DateTimeOffset(
+                        2021, 04, 17, 20, 20, 34, 
+                        TimeSpan.Zero
+                        ),
+                    "You overslept the alarm clock!!!",
+                    "You don't have to go to work today",
+                    "Nikolay"
+                    )
+            };
+
+            foreach (Reminderltem reminderltem in reminderltems)
+            {
+                reminderltem.WriteProperties();
+            }
+        }
+    }
+}
