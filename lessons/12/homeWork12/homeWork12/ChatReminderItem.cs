ef670544dadac878c404771ab70bc329f05fb768
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-19 23:10:46 +0300

    Класс ChatReminderItem

---
+++ lessons/12/homeWork12/homeWork12/ChatReminderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace homeWork12
+{
+    class ChatReminderItem : Reminderltem
+    {
+        public string ChatName { get; set; }
+        public string AccountName { get; set; }
+        public override void WriteProperties()
+        {
+            Console.WriteLine($"ChatReminderItem:" +
+                $" \n  Chat name: {ChatName}" +
+                $" and account name: {AccountName}");
+        }
+        public ChatReminderItem(
+            DateTimeOffset alarmDate,
+            string alarmMessange,
+            string chatName,
+            string accountName
+            ) 
+            : base(alarmDate, alarmMessange)
+        {
+            ChatName = chatName;
+            AccountName = accountName;
+        }
+    }
+}
