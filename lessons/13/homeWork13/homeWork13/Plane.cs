dbc4bb5fc1b25476b26ef9970a95eecde25c7728
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-25 22:00:27 +0300

    Plane

---
+++ lessons/13/homeWork13/homeWork13/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homeWork13
+{
+    class Plane : AirTransport
+    {
+        public byte EnginesCount { get; private set; }
+        public Plane(int maxHeight, byte enginesCount)
+        {
+            CurrentHeight = 0;
+            MaxHeight = maxHeight;
+            EnginesCount = enginesCount;
+            Console.WriteLine("It's a plane, welcome aboard");
+        }
+        public override void WriteAllPropertise()
+        {
+            Console.WriteLine("Propertise of Helicopter:");
+            base.WriteAllPropertise2();
+            Console.WriteLine($"EnginesCount: {EnginesCount}\n");
+        }
+    }
+}
