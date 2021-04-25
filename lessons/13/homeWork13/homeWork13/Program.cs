6d01754e0fc624dfb557e22ba365540696036d2b
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-25 21:58:17 +0300

    ДЗ-13

---
+++ lessons/13/homeWork13/homeWork13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homeWork13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane plane1 = new Plane(10000, 4);
+            plane1.WriteAllPropertise();
+
+            Helicopter helicopter1 = new Helicopter(5000, 6);
+            helicopter1.WriteAllPropertise();
+        }
+    }
+}
