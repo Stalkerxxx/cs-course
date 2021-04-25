c61f0c6edb6679501307cabf85982a1b7134ceab
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-25 22:01:07 +0300

    Helicopter

---
+++ lessons/13/homeWork13/homeWork13/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homeWork13
+{
+    class Helicopter : AirTransport
+    {
+        public byte BladesCount { get; private set; }
+        public Helicopter(int maxHeight, byte bladesCount)
+        {
+            CurrentHeight = 0;
+            MaxHeight = maxHeight;
+            BladesCount = bladesCount;
+                Console.WriteLine("It's a helicopter, welcome aboard");
+        }
+
+        public override void WriteAllPropertise() 
+        {
+            Console.WriteLine("Propertise of Helicopter:");
+            base.WriteAllPropertise2();
+            Console.WriteLine($"EnginesCount: {BladesCount}");
+        }
+
+    }
+}
