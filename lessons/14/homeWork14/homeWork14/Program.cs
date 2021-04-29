eca548419f208cf93dc7f547595277bb32206869
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-29 22:56:38 +0300

    Дз-14

---
+++ lessons/14/homeWork14/homeWork14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace homeWork14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLogWriter c = new ConsoleLogWriter();
+            c.LogInfo("LOOL");
+            c.LogWarning("Keke");
+            c.LogError("SOS");
+
+            FileLogWriter f = new FileLogWriter();
+            f.LogError("LOL");
+            f.LogInfo("Hello World");
+            f.LogWarning("Salam");
+
+            MultipleLogWriter m = new MultipleLogWriter(new List<MultipleLogWriter> { });
+            m.LogError("LOL");
+            m.LogInfo("KOLYA");
+            m.LogWarning("SAYOP");
+        }
+    }
+}
