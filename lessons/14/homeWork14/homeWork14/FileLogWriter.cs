b9a9ecd6cb7ea0799fc644d8773de69d062af33a
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-04-29 22:58:11 +0300

    FileLogWriter

---
+++ lessons/14/homeWork14/homeWork14/FileLogWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace homeWork14
+{
+    class FileLogWriter : ILogWriter
+    {
+        public string MessageType { get; set; }
+        public void LogInfo(string message)
+        {
+            MessageType = "Info";
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+        }
+
+        public void LogWarning(string message)
+        {
+            MessageType = "Warning";
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+        }
+
+        public void LogError(string message)
+        {
+            MessageType = "Error";
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+        }
+    }
+}
