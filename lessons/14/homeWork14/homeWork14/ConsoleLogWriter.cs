7abd2482d0f0c20943938bd74fe7699ddf93316f
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-05-10 13:10:57 +0300

    ConsoleLog

--- lessons/14/homeWork14/homeWork14/ConsoleLogWriter.cs
+++ lessons/14/homeWork14/homeWork14/ConsoleLogWriter.cs
@@ -4,23 +4,19 @@ namespace homeWork14
 {
     class ConsoleLogWriter : ILogWriter
     {
-        public string MessageType { get; set; }
         public void LogInfo(string message)
         {
-            MessageType = "Info";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
+            Console.WriteLine($"{DateTimeOffset.Now} {"Info"} {message}");
         }
 
         public void LogWarning(string message) 
         {
-            MessageType = "Warning";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
+            Console.WriteLine($"{DateTimeOffset.Now} {"Warning"} {message}");
         }
 
         public void LogError(string message)
         {
-            MessageType = "Error";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
+            Console.WriteLine($"{DateTimeOffset.Now} {"Error"} {message}");
         }
     }
 }
