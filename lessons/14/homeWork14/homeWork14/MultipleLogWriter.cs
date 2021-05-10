8d4fff6490f3b7dfd9c8c598f1e4a709fa9c29ad
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-05-10 13:12:07 +0300

    MultipleLogWriter

--- lessons/14/homeWork14/homeWork14/MultipleLogWriter.cs
+++ lessons/14/homeWork14/homeWork14/MultipleLogWriter.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace homeWork14
 {
     class MultipleLogWriter : ILogWriter
     {
-        public string MessageType { get; set; }
-        public List<MultipleLogWriter> LogWriter { get; set; }
+        private readonly List<ILogWriter> _logWriter = new List<ILogWriter> {new ConsoleLogWriter(), new FileLogWriter()};
         public void LogInfo(string message)
         {
-            MessageType = "Info";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
-            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+            Console.WriteLine($"{DateTimeOffset.Now} {"Info"} {message}");
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {"Info"} {message}\n");
         }
 
         public void LogWarning(string message)
         {
-            MessageType = "Warning";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
-            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+            
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {"Warning"} {message}\n");
         }
 
         public void LogError(string message)
         {
-            MessageType = "Error";
-            Console.WriteLine($"{DateTimeOffset.Now} {MessageType} {message}");
-            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {MessageType} {message}\n");
+            Console.WriteLine($"{DateTimeOffset.Now} {"Error"} {message}");
+            File.AppendAllText("somefile.txt", $"{DateTimeOffset.Now} {"Error"} {message}\n");
         }
 
-        public MultipleLogWriter(List<MultipleLogWriter> logWriters)
+        public MultipleLogWriter(List<ILogWriter> logWriters)
         {
             LogWriter = logWriters;
         }
