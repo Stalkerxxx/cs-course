762081812f4f18deb6863e5fa6a04d67abd087a2
Stalkerxxx <knyazev.nikolay@yandex.ru>
2021-05-24 20:59:19 +0300

    Operation

---
+++ lessons/16/homeWork/homeWork16/Calculator.Operation/CircleOperation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Calculator.Operation
+{
+    public class CircleOperation
+    {
+        public static double Perimeter(double radius) => 2 * radius * Math.PI;
+        public static double Square(double radius) => Math.PI * Math.Pow(radius, 2);
+    }
+}
---
+++ lessons/16/homeWork/homeWork16/Calculator.Operation/SquareOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Operation
+{
+    public class SquareOperation
+    {
+        public static double Perimeter(double side) => 4 * side;
+        public static double Square(double side) => Math.Pow(side, 2);
+    }
+}
